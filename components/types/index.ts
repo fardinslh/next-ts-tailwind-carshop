a39c50e5a41e5367c06425e75ad8f572b6837a6b
FardinSalehi <fardinsalehizadeh1998@gmail.com>
2023-10-16 10:28:40 +0330

    add SearchBar, SearchManufacturer and CustomFilter components

--- components/types/index.ts
+++ components/types/index.ts
@@ -7,3 +7,8 @@ export interface CustomButtonProps {
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
